349228b4750434280af0f8861a9965f63a5dd913
Shaharyar Ahmad <shaharyarr.ahmad@gmail.com>
2022-01-06 05:46:05 +0500

    test-project

---
+++ components/defineJobs.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="bg mb-11">
+    <h1 class="p-5">Deine Job website</h1>
+    <img src="/1.svg" class="center m-0 p-0">
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#fff" fill-opacity="1" d="M0,320L60,314.7C120,309,240,299,360,261.3C480,224,600,160,720,122.7C840,85,960,75,1080,101.3C1200,128,1320,192,1380,224L1440,256L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path></svg>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+<style scoped>
+.bg {
+  background: transparent linear-gradient(141deg, #EBF4FF 0%, #E6FFFA 100%) 0% 0% no-repeat padding-box;
+  overflow: visible;
+}
+  h1{
+text-align: center;
+font: normal normal 600 42px/50px lato;
+letter-spacing: 1.26px;
+color: #2D3748;
+  }
+.center {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+
+}
+
+</style>
