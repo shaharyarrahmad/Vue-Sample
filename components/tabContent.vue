096bc2b4b0edad79f919d9c875b23773cfc049d3
Shaharyar Ahmad <shaharyarr.ahmad@gmail.com>
2022-01-06 06:43:40 +0500

    fixes

--- components/tabContent.vue
+++ components/tabContent.vue
@@ -18,7 +18,7 @@
     </p>
   <p class="text text2 mb-6 ml-5">{{data.labels[1]}}</p>
   </div>
-  <img class="-mb-10 img2" :src="data.images[1]" :class="data.images[1].includes('3.2') ? 'pl-20 pr-5':(data.images[1].includes('2.2')? 'pl-10 img2 pr-10 pb-10':'pl-24 pr-11')" />
+  <img class="-mb-10 " :src="data.images[1]" :class="data.images[1].includes('3.2') ? 'pl-20 pr-5':(data.images[1].includes('2.2')? 'pl-10 img2 pr-10 pb-10':'pl-24 pr-11')" />
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" class="-m-5 mt-5 custom-layer">
     <path fill="#fff" fill-opacity="1" class="custom-layer" d="M0,64L60,80C120,96,240,128,360,160C480,192,600,224,720,218.7C840,213,960,171,1080,144C1200,117,1320,107,1380,101.3L1440,96L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path>
 
